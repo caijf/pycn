f6a66ecc159274edd302dae7ec2a45a013bd2430
蔡金锋 <jeff.cai@99bill.com>
2021-11-12 14:37:44 +0800

    基础功能完成

---
+++ scripts/generator.js
@@ -0,0 +1,129 @@
+const path = require('path');
+const fse = require('fs-extra');
+const pyData = require('../data/py-data.json');
+
+const LIB_DIR = path.join(__dirname, '../lib');
+
+const createUMD = async (fileName, moduleName, exportsCode) => {
+  const code = `(function (root, factory) {
+  if (typeof exports === 'object' && typeof module !== 'undefined') {
+    module.exports = factory();
+  } else if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else {
+    root.${moduleName} = factory();
+  }
+}(this, function () {
+  return ${exportsCode};
+}));`;
+
+  const filePath = path.join(LIB_DIR, fileName);
+
+  try {
+    await fse.ensureDir(LIB_DIR);
+    await fse.outputFile(filePath, code);
+    console.log(`done! ${filePath}`);
+  } catch (err) {
+    console.log(`error! ${filePath}`);
+    console.log(err);
+  }
+};
+
+const LEVEL_FLAG = {
+  PROVINCE: 'province',
+  CITY: 'city',
+  DISTRICT: 'district',
+  STREET: 'street'
+}
+
+const province = [];
+const city = [];
+const district = [];
+const street = [];
+let province_city = [];
+let province_city_district = [];
+
+const trans = (dists) => {
+  dists.forEach(item => {
+    const { districts, level, ...rest } = item;
+    if (level === LEVEL_FLAG.PROVINCE) {
+      province.push(rest);
+    } else if (level === LEVEL_FLAG.CITY) {
+      city.push(rest);
+    } else if (level === LEVEL_FLAG.DISTRICT) {
+      district.push(rest);
+    } else if (level === LEVEL_FLAG.STREET) {
+      street.push(rest);
+    }
+
+    if (Array.isArray(districts) && districts.length > 0) {
+      trans(districts);
+    }
+  })
+}
+
+const transCascade = (dists, untilLevel) => {
+  return dists.map(item => {
+    const { districts, level, ...rest } = item;
+    if (level === untilLevel) {
+      return rest;
+    }
+
+    if (Array.isArray(districts) && districts.length > 0) {
+      return {
+        ...rest,
+        districts: transCascade(districts, untilLevel)
+      }
+    }
+
+    return {
+      ...rest,
+      districts
+    };
+  })
+}
+
+trans(pyData);
+
+province_city = transCascade(pyData, LEVEL_FLAG.CITY);
+province_city_district = transCascade(pyData, LEVEL_FLAG.DISTRICT);
+
+// 省
+const PY_PROVINCE_MODULE = 'pyProvince';
+const PY_PROVINCE_FILE = 'py-province.js';
+
+// 市
+const PY_CITY_MODULE = 'pyCity';
+const PY_CITY_FILE = 'py-city.js';
+
+// 区
+const PY_DISTRICT_MODULE = 'pyDistrict';
+const PY_DISTRICT_FILE = 'py-district.js';
+
+// 街道
+const PY_STREET_MODULE = 'pyStreet';
+const PY_STREET_FILE = 'py-street.js';
+
+// 省市
+const PY_PROVINCE_CITY_MODULE = 'pyProvinceCity';
+const PY_PROVINCE_CITY_FILE = 'py-province-city.js';
+
+// 省市区
+const PY_PROVINCE_CITY_DISTRICT_MODULE = 'pyProvinceCityDistrict';
+const PY_PROVINCE_CITY_DISTRICT_FILE = 'py-province-city-district.js';
+
+// 省市区街道
+const PY_DATA_MODULE = 'pyData';
+const PY_DATA_FILE = 'py-data.js';
+
+const run = async () => {
+  await createUMD(PY_PROVINCE_FILE, PY_PROVINCE_MODULE, JSON.stringify(province));
+  await createUMD(PY_CITY_FILE, PY_CITY_MODULE, JSON.stringify(city));
+  await createUMD(PY_DISTRICT_FILE, PY_DISTRICT_MODULE, JSON.stringify(district));
+  await createUMD(PY_STREET_FILE, PY_STREET_MODULE, JSON.stringify(street));
+  await createUMD(PY_PROVINCE_CITY_FILE, PY_PROVINCE_CITY_MODULE, JSON.stringify(province_city));
+  await createUMD(PY_PROVINCE_CITY_DISTRICT_FILE, PY_PROVINCE_CITY_DISTRICT_MODULE, JSON.stringify(province_city_district));
+  await createUMD(PY_DATA_FILE, PY_DATA_MODULE, JSON.stringify(pyData));
+}
+
+run();
---
+++ scripts/init.js
@@ -0,0 +1,82 @@
+const fse = require('fs-extra');
+const path = require('path');
+const { Readable } = require('stream');
+const { FormDataEncoder } = require('form-data-encoder');
+const pinyin4js = require('pinyin4js');
+const { FormData } = require('formdata-node');
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
+
+const url = 'https://lbs.amap.com/service/api/restapi?keywords=中国&subdistrict=4&extensions=base';
+
+const form = new FormData();
+form.set('type', 'config/district');
+form.set('version', 'v3');
+
+const encoder = new FormDataEncoder(form);
+
+// 生成随机IP， 赋值给 X-Forwarded-For
+function getRandomIP() {
+  return Array.from(Array(4)).map(() => parseInt(Math.random() * 255)).join('.')
+}
+
+const options = {
+  method: "post",
+  headers: {
+    ...encoder.headers,
+    'X-Forwarded-For': getRandomIP(),
+    rejectUnauthorized: false,
+    'Postman-Token': Date.now(),
+    // 'Cache-Control': 'no-cache',
+    'User-Agent':
+      'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2924.87 Safari/537.36'
+  },
+  body: Readable.from(encoder)
+}
+
+const fetchData = () => fetch(url, options);
+
+const transformPy = (district) => {
+  let newDistrict = {};
+  if (typeof district === 'object' && district.name) {
+    const { citycode, center, districts, ...rest } = district;
+    newDistrict = {
+      ...rest,
+      py: pinyin4js.convertToPinyinString(district.name, '', pinyin4js.WITHOUT_TONE)
+    };
+    if (Array.isArray(districts) && districts.length > 0) {
+      newDistrict.districts = districts.map(transformPy);
+    }
+  }
+  return newDistrict;
+}
+
+const DATA_DIR = path.join(__dirname, '../data');
+const DATA_FILE = path.join(DATA_DIR, 'data.json');
+const PY_DATA_FILE = path.join(DATA_DIR, 'py-data.json');
+
+const run = async () => {
+  try {
+    const response = await fetchData();
+
+    const json = await response.json();
+    const text = JSON.stringify(json);
+
+    if (json?.status !== '1') {
+      throw json;
+    }
+
+    await fse.ensureDir(DATA_DIR);
+
+    // 写入data/data.json
+    await fse.outputFile(DATA_FILE, text);
+
+    // 写入data/py-data.json
+    const districts = json.districts[0].districts;
+    const pyResult = districts.map(transformPy);
+    await fse.outputFile(PY_DATA_FILE, JSON.stringify(pyResult));
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+run();
